b45e23539e0312f82541c363ef32d97d65ecfb0b
aebinessa <abinessa0004@sdsu.edu>
2024-03-03 15:36:38 +0300

    finishedtask

--- NavigationTask/SecondViewController.swift
+++ NavigationTask/SecondViewController.swift
@@ -11,19 +11,13 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Second View"
+
 
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink // Easily distinguishable background color
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- NavigationTask/ViewController.swift
+++ NavigationTask/ViewController.swift
@@ -6,14 +6,58 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    let showDetailsButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        title = "First View"
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithDefaultBackground()
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+
+        setupViews()
+        setupUI()
+        setupConstraints()
+        
+        showDetailsButton.addTarget(self, action: #selector(showDetailsButtonTapped), for: .touchUpInside)
     }
-
-
+    
+    @objc func showDetailsButtonTapped() {
+        let secondVC = SecondViewController()
+        secondVC.modalPresentationStyle = .fullScreen // Present full screen
+        self.navigationController?.pushViewController(secondVC, animated: true)           }
+    
+    
+    func setupViews(){
+        view.addSubview(showDetailsButton)
+        view.backgroundColor = .white
+    }
+    func setupUI(){
+        showDetailsButton.setTitle("Show Details", for: .normal)
+        showDetailsButton.setTitleColor(.white, for: .normal)
+        showDetailsButton.backgroundColor = UIColor.blue
+        showDetailsButton.layer.cornerRadius = 8
+        
+        
+    }
+    func setupConstraints(){
+        showDetailsButton.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.equalTo(200)
+            make.height.equalTo(100)
+        }
+        
+    }
+    
 }
+
+
+
+
+
 
